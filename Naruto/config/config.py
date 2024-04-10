277e9f7986e9d2ab3619cd82eecf627afa125cd0
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-10 15:17:11 +0530

    Update config.py

--- Naruto/config/config.py
+++ Naruto/config/config.py
@@ -1,14 +1,14 @@
 # API credentials
-API_ID = "24545226"
-API_HASH = "c9cc641ca738adf1ea5b29f3dd99315d"
-BOT_TOKEN = "6206599982:AAGMtdPmgw7wRF5EsdREiJT3Qb9Yy8ALIVE"
+API_ID = "20457610"
+API_HASH = "b7de0dfecd19375d3f84dbedaeb92537"
+BOT_TOKEN = "7086066440:AAGtDkNG4KdvIEPVwO0IQvQHP0rcmTl9rYA"
 
 # Database configuration
-MONGO_URI = "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://vinamratiwari579:m6YDRYH8HbwuEqxt@cluster0.x7ac1wt.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "naruto_game_db"
 
 # Download directory for temporary files
 DOWNLOAD_DIRECTORY = "./"
 
 # Support chat information
-SUPPORT_CHAT = "NanoSTestingArea"
+SUPPORT_CHAT = "naruto_support1"
